518758daccde6b4e8461144ba2aa1a6be03a085d
Dark <kilometro0ec@gmail.com>
2026-04-28 13:57:39 -0500

    Add footer version display on DOMContentLoaded

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const version = "0.0.5";
+
+  document.querySelector(".footer").innerHTML =
+    `by CORPORACIÓN 360 | V.${version}`;
+
+});
