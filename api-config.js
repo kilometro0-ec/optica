925519d2c2e8963cf75eeaa49bbf8936d406ccca
Dark <kilometro0ec@gmail.com>
2026-04-28 13:53:12 -0500

    Add Firebase configuration and initialization

---
+++ api-config.js
@@ -0,0 +1,12 @@
+// Configuración base (reemplazar con tus datos reales)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "opticontrol.firebaseapp.com",
+  projectId: "opticontrol",
+  storageBucket: "opticontrol.appspot.com",
+  messagingSenderId: "XXXXX",
+  appId: "XXXXX"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
